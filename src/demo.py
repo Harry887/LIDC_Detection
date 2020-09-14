061780970f8e74b9fce93cb8008e9595633b19d4
Harry887 <761228040@qq.com>
2020-09-14 10:22:41 -0400

    load dicom file

--- src/demo.py
+++ src/demo.py
@@ -1,28 +1,85 @@
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd
 import pydicom
 import pylidc as pl
 import os
 import os.path as osp
 import scipy.ndimage
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 from skimage import measure, morphology
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from collections import Counter
 
 
-def load_scan(path):
-    slices = [pydicom.read_file(path + '/' + s) for s in os.listdir(path)]
-    print(slices)
+def load_scans(dcm_path):
+    files = os.listdir(dcm_path)
+    sorted_file = np.sort(files)
+    f = [pydicom.dcmread(dcm_path + "/" + str(file))
+         for file in sorted_file if file.endswith(".dcm")]
+    return f
+
+
+def plot_HU_range(scans, name):
+    fig, ax = plt.subplots(1, 2, figsize=(20, 5))
+    for n in range(10):
+        image = scans[n].pixel_array.flatten()
+        rescaled_image = image * \
+            scans[n].RescaleSlope + scans[n].RescaleIntercept
+        sns.distplot(image.flatten(), ax=ax[0])
+        sns.distplot(rescaled_image.flatten(), ax=ax[1])
+    ax[0].set_title("Raw pixel array distributions for 10 examples")
+    ax[1].set_title("HU unit distributions for 10 examples")
+    plt.savefig("demo/"+name+".png")
+
+
+def transform_to_hu(slices):
+    images = np.stack([file.pixel_array for file in slices])
+    images = images.astype(np.int16)
+
+    # convert ouside pixel-values to air:
+    # I'm using <= -1000 to be sure that other defaults are captured as well
+    images[images <= -1000] = 0
+
+    # convert to HU
+    for n in range(len(slices)):
+
+        intercept = slices[n].RescaleIntercept
+        slope = slices[n].RescaleSlope
+
+        if slope != 1:
+            images[n] = slope * images[n].astype(np.float64)
+            images[n] = images[n].astype(np.int16)
+
+        images[n] += np.int16(intercept)
+
+    return np.array(images, dtype=np.int16)
 
 
 def load_patients(dataset_path):
+    """Return patients name list like: LIDC-IDRI-XXXX
+    """
     patients = os.listdir(dataset_path)
     return sorted(patients)
 
 
+def series_modality(series_path):
+    """Return modality type of series
+    """
+    slice_demo = osp.join(series_path, os.listdir(series_path)[0])
+    slice_info = load_scan(slice_demo)
+    return slice_info['Modality'].value
+
+
+def modality_filter():
+    """Filt out other modality except for CT
+    """
+    pass
+
+
 def count_files(path):
+    """Count files under the path of folder
+    """
     file_num = 0
     for root, dirs, files in os.walk(path):
         file_num += len(files)
--- src/dicom_tag.py
+++ src/dicom_tag.py
@@ -1,5 +1,36 @@
 import pydicom
+import pylidc as pl
 
-ds = pydicom.read_file(
-    "data/LIDC/LIDC-IDRI/LIDC-IDRI-0001/01-01-2000-30178/3000566-03192/000001.dcm")
-print(ds)
+
+def read_dicom():
+    ds = pydicom.read_file(
+        "data/LIDC/LIDC-IDRI/LIDC-IDRI-0001/01-01-2000-30178/3000566-03192/000001.dcm")
+    print(ds)
+
+
+def query():
+    scans = pl.query(pl.Scan).filter(pl.Scan.slice_thickness <= 1,
+                                     pl.Scan.pixel_spacing <= 0.6)
+    print(scans.count())
+    pid = 'LIDC-IDRI-0078'
+    scan = pl.query(pl.Scan).filter(pl.Scan.patient_id == pid).first()
+    print(scan)
+    print(len(scan.annotations))
+
+    nods = scan.cluster_annotations()
+    print("%s has %d nodules." % (scan, len(nods)))
+    # => Scan(id=1,patient_id=LIDC-IDRI-0078) has 4 nodules.
+
+    for i, nod in enumerate(nods):
+        print("Nodule %d has %d annotations." % (i+1, len(nods[i])))
+    vol = scan.to_volume()
+    print(vol.shape)
+    # => (512, 512, 87)
+
+    print("%.2f, %.2f" % (vol.mean(), vol.std()))
+    # => -702.15, 812.52
+    scan.visualize(annotation_groups=nods)
+
+
+if __name__ == "__main__":
+    query()
