1deb51e16d9e1e387bf4e36da4f2e7d58b5780bb
Harry887 <761228040@qq.com>
2020-09-14 10:23:21 -0400

    test for HU range

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,8 +1,19 @@
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from src.demo import count_files
+from src.demo import load_scans, count_files, plot_HU_range
+
+
+def test_load_scans():
+    slice_path = "/home1/hli/xiongweiyu/data/LIDC/LIDC-IDRI/LIDC-IDRI-0001/01-01-2000-30178/3000566-03192/"
+    slice_info = load_scans(slice_path)
+    print(slice_info)
+
 
 def test_count_files():
     path = "/home1/hli/xiongweiyu/data/LIDC/LIDC-IDRI/LIDC-IDRI-0106/"
     assert count_files(path) == 159
+
+
+def test_plot_HU_range():
+    slice_path = "/home1/hli/xiongweiyu/data/LIDC/LIDC-IDRI/LIDC-IDRI-0001/01-01-2000-30178/3000566-03192/"
+    file_name = "HU_range"
+    scans = load_scans(slice_path)
+    plot_HU_range(scans, file_name)
